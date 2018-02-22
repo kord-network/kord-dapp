eb8feec0b58faee98a328a4305f6b90f6c935824
lukehedger <luke.hedger@gmail.com>
2018-02-22 14:57:23 +0000

    Add profile domain

--- app/core/reducers/index.js
+++ app/core/reducers/index.js
@@ -2,10 +2,12 @@ import { foundReducer } from 'found'
 import { combineReducers } from 'redux-immutablejs'
 
 import * as MarketPlace from 'domains/marketplace'
+import * as Profile from 'domains/profile'
 import * as UI from 'domains/ui'
 
 export default combineReducers({
   found: foundReducer,
   [MarketPlace.name]: MarketPlace.reducer,
+  [Profile.name]: Profile.reducer,
   [UI.name]: UI.reducer,
 })
