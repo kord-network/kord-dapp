2982a4a14931a2d302b6883a0fa877613fed3a6a
lukehedger <luke.hedger@gmail.com>
2018-02-22 15:12:14 +0000

    Add session middleware: - Read claims on login - Redirect to Home page on login

--- app/core/middleware/index.js
+++ app/core/middleware/index.js
@@ -1,14 +1,12 @@
 import { createLogger } from 'redux-logger'
 import { middleware as reduxPack } from 'redux-pack'
 
+import SessionMiddleware from './session'
 import UIMiddleware from './ui'
 
 const createMiddleware = isDevelopment => {
   // default middleware
-  const middleware = [
-    reduxPack,
-    UIMiddleware,
-  ]
+  const middleware = [reduxPack, SessionMiddleware, UIMiddleware]
 
   // logger middleware in development
   if (isDevelopment) middleware.push(createLogger({ collapsed: true }))
