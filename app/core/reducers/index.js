2c6692faf91cac82a77b9ad8f345838d824afa67
lukehedger <luke.hedger@gmail.com>
2018-02-22 12:18:14 +0000

    Add claims domain

--- app/core/reducers/index.js
+++ app/core/reducers/index.js
@@ -1,9 +1,11 @@
 import { foundReducer } from 'found'
 import { combineReducers } from 'redux-immutablejs'
 
+import * as Claims from 'domains/claims'
 import * as UI from 'domains/ui'
 
 export default combineReducers({
   found: foundReducer,
+  [Claims.name]: Claims.reducer,
   [UI.name]: UI.reducer,
 })
