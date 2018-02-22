aff3e7733b67f4063749c0dba11df55acade505d
lukehedger <luke.hedger@gmail.com>
2018-02-22 14:53:00 +0000

    Add identity domain

--- app/core/reducers/index.js
+++ app/core/reducers/index.js
@@ -1,11 +1,13 @@
 import { foundReducer } from 'found'
 import { combineReducers } from 'redux-immutablejs'
 
+import * as Identity from 'domains/identity'
 import * as MarketPlace from 'domains/marketplace'
 import * as UI from 'domains/ui'
 
 export default combineReducers({
   found: foundReducer,
+  [Identity.name]: Identity.reducer,
   [MarketPlace.name]: MarketPlace.reducer,
   [UI.name]: UI.reducer,
 })
