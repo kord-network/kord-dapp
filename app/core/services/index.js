e32da77a66caf3ba337fd9f8641a8b284493b50c
lukehedger <luke.hedger@gmail.com>
2018-02-22 11:41:59 +0000

    Add profile claim Swarm hash resolution service

--- app/core/services/index.js
+++ app/core/services/index.js
@@ -1,9 +1,10 @@
 import * as Facebook from './facebook'
 import * as Fetch from './fetch'
+import * as KordClaims from './kord-claims'
 import * as KordId from './kord-id'
 import * as KordNetwork from './kord-network'
 import Sentry from './sentry'
 import * as Swarm from './swarm'
 import * as WebStorage from './web-storage'
 
-export { Facebook, Fetch, KordId, KordNetwork, Sentry, Swarm, WebStorage }
+export { Facebook, Fetch, KordClaims, KordId, KordNetwork, Sentry, Swarm, WebStorage }
