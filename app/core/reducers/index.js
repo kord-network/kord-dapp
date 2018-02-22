2296cc1856ff76e77f2077137d8b99a6767403f9
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-02-22 12:09:58 +0000

    marketplace: Change reducer domain import and state defition to alphabetic order [Delivers #155404946]

--- app/core/reducers/index.js
+++ app/core/reducers/index.js
@@ -1,11 +1,11 @@
 import { foundReducer } from 'found'
 import { combineReducers } from 'redux-immutablejs'
 
-import * as UI from 'domains/ui'
 import * as MarketPlace from 'domains/marketplace'
+import * as UI from 'domains/ui'
 
 export default combineReducers({
   found: foundReducer,
-  [UI.name]: UI.reducer,
   [MarketPlace.name]: MarketPlace.reducer,
+  [UI.name]: UI.reducer,
 })
