05c5a41f8f4fab709f3c2113290f78a2f160ec9f
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-02-22 22:06:17 +0000

    marketplace: Add dispatcher for retrieval of marketplace, via middleware, data on login. [Delivers #155405906]

--- app/core/middleware/session.js
+++ app/core/middleware/session.js
@@ -4,6 +4,7 @@ import { routes } from 'core/routes'
 import { isDomainAction } from 'core/util'
 import { actions as ClaimsActions } from 'domains/claims'
 import { actionTypes as session, name } from 'domains/session'
+import { actions as MarketActions } from 'domains/marketplace'
 
 const SessionMiddleware = ({ dispatch }) => next => action => {
   // action not in namespace? abort!
@@ -12,6 +13,9 @@ const SessionMiddleware = ({ dispatch }) => next => action => {
   if (session.LOGIN === action.type) {
     // fetch user's KORD Claims Graph
     dispatch(ClaimsActions.readClaimsByGraph(action.payload.graph))
+
+    // fetch marketplaces from KORD network
+    dispatch(MarketActions.getAllMarketplaces())
 
     // redirect to Home page
     dispatch(farce.push(routes.home.path))
