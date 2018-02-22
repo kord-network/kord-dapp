43fcf4e6478369449ec64696c8ef6c8dd69ed4a5
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-02-22 10:47:31 +0000

    Marketplace: Add marketplace domain and add to state [Delivers #155404946]

--- app/core/reducers/index.js
+++ app/core/reducers/index.js
@@ -2,8 +2,10 @@ import { foundReducer } from 'found'
 import { combineReducers } from 'redux-immutablejs'
 
 import * as UI from 'domains/ui'
+import * as MarketPlace from 'domains/marketplace'
 
 export default combineReducers({
   found: foundReducer,
   [UI.name]: UI.reducer,
+  [MarketPlace.name]: MarketPlace.reducer,
 })
