194435f46a4d1683ff09c59501f96979b9847606
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-02-25 17:23:37 +0000

    marketplace: Adds marketplace middleware and service to get service provider profile claims [Delivers #155358824]

--- app/core/middleware/index.js
+++ app/core/middleware/index.js
@@ -1,12 +1,18 @@
 import { createLogger } from 'redux-logger'
 import { middleware as reduxPack } from 'redux-pack'
 
+import MarketplaceMiddleware from './marketplace'
 import SessionMiddleware from './session'
 import UIMiddleware from './ui'
 
 const createMiddleware = isDevelopment => {
   // default middleware
-  const middleware = [reduxPack, SessionMiddleware, UIMiddleware]
+  const middleware = [
+    reduxPack,
+    MarketplaceMiddleware,
+    SessionMiddleware,
+    UIMiddleware,
+  ]
 
   // logger middleware in development
   if (isDevelopment) middleware.push(createLogger({ collapsed: true }))
