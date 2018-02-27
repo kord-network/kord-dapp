29f8085105fd2d1fdd2a8d3b3112e1de2520027f
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-02-27 21:17:45 +0000

    misc: Change kord-id service to kord-graph, references to kord-id to be removed

--- app/core/services/index.js
+++ app/core/services/index.js
@@ -1,11 +1,21 @@
 import * as Facebook from './facebook'
 import * as Fetch from './fetch'
 import * as KordClaims from './kord-claims'
-import * as KordId from './kord-id'
+import * as KordGraph from './kord-graph'
 import * as KordNetwork from './kord-network'
 import * as Marketplace from './marketplace'
 import Sentry from './sentry'
 import * as Swarm from './swarm'
 import * as WebStorage from './web-storage'
 
-export { Facebook, Fetch, KordClaims, KordId, KordNetwork, Marketplace, Sentry, Swarm, WebStorage }
+export {
+  Facebook,
+  Fetch,
+  KordClaims,
+  KordGraph,
+  KordNetwork,
+  Marketplace,
+  Sentry,
+  Swarm,
+  WebStorage,
+}
