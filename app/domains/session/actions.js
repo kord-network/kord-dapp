b8f0b9fc2475bb39ba2affaf8efa714e78e225dc
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-02-28 14:07:33 +0000

    session: Remove argument manipulation from unlockAccount action

--- app/domains/session/actions.js
+++ app/domains/session/actions.js
@@ -1,20 +1,17 @@
-import { accounts } from 'core/util'
-
 import * as actions from './actionTypes'
 
 /**
  * Start session and set account
  *
- * @param  {Object}  keystore          Encrypted Ethereum keystore
- * @param  {String}  password          Password for the keystore
- * @return {Object}                    Flux Standard Action
+ * @param  {Object}  keystore          		Encrypted Ethereum keystore object
+ * @param  {Object}  account           		Ethereum account object
+ * @param  {String}  account.address   		Public address
+ * @param  {String}  account.privateKey		Private key
+ * @return {Object}                    		Flux Standard Action
  */
-export const unlockAccount = (keystore, password) => ({
+export const unlockAccount = (keystore, account) => ({
   type: actions.UNLOCK_ACCOUNT,
-  payload: {
-    keystore: JSON.parse(keystore),
-    account: accounts.create(JSON.parse(keystore), password),
-  },
+  payload: { keystore, account },
 })
 
 /**
