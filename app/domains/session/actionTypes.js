62aec6bdb852ba9ae2d728abdae88b156c8a5664
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-02-28 13:17:21 +0000

    session: Change to mergeDeep in unlockAccount case in session reducer and alphabetize

--- app/domains/session/actionTypes.js
+++ app/domains/session/actionTypes.js
@@ -1,5 +1,5 @@
 import { name } from './constants'
 
-export const UNLOCK_ACCOUNT = `${name}/UNLOCK_ACCOUNT`
 export const SET_IS_NEW_USER = `${name}/SET_IS_NEW_USER`
 export const SET_OAUTH_CLAIM_MESSAGE = `${name}/SET_OAUTH_CLAIM_MESSAGE`
+export const UNLOCK_ACCOUNT = `${name}/UNLOCK_ACCOUNT`
