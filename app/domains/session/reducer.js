50864689189ec2ab35dab080ef25ab6f163d9f8f
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-02-28 13:17:21 +0000

    session: Change to mergeDeep in unlockAccount case in session reducer and alphabetize

--- app/domains/session/reducer.js
+++ app/domains/session/reducer.js
@@ -8,23 +8,23 @@ const createAccount = account => model.accountFactory(account)
 
 export const initialState = {
   account: null,
-  encryptedkeystores: null,
+  encryptedKeystores: null,
   graph: null,
   isNewUser: false,
   oAuthClaimMessage: null,
 }
 
 export default createReducer(fromJS(initialState), {
-  [actions.UNLOCK_ACCOUNT]: (state, action) =>
-    state.merge({
-      account: createAccount(action.payload.account),
-      encryptedkeystores: {
-        [action.payload.account.address]: action.payload.keystore,
-      },
-    }),
-
   [actions.SET_IS_NEW_USER]: (state, action) => state.merge(action.payload),
 
   [actions.SET_OAUTH_CLAIM_MESSAGE]: (state, action) =>
     state.merge(action.payload),
+
+  [actions.UNLOCK_ACCOUNT]: (state, action) =>
+    state.mergeDeep({
+      account: createAccount(action.payload.account),
+      encryptedKeystores: {
+        [action.payload.account.address]: action.payload.keystore,
+      },
+    }),
 })
