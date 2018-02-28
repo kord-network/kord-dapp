170fa1f859c27b98515755de2d7d6eb50f653c9b
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-02-28 15:48:14 +0000

    middleware: Remove dispatches to surplus actions

--- app/core/middleware/session.js
+++ app/core/middleware/session.js
@@ -1,10 +1,6 @@
-import { Actions as farce } from 'farce'
-
-import { routes } from 'core/routes'
 import { isDomainAction } from 'core/util'
 import { actions as ClaimsActions } from 'domains/claims'
 import { actionTypes as session, name } from 'domains/session'
-import { actions as MarketActions } from 'domains/marketplace'
 
 const SessionMiddleware = ({ dispatch }) => next => action => {
   // action not in namespace? abort!
@@ -13,12 +9,6 @@ const SessionMiddleware = ({ dispatch }) => next => action => {
   if (session.UNLOCK_ACCOUNT === action.type) {
     // fetch user's KORD Claims Graph
     dispatch(ClaimsActions.readClaimsByGraph(action.payload.account.address))
-
-    // fetch marketplaces from KORD network
-    dispatch(MarketActions.getMarketplaces())
-
-    // redirect to Home page
-    dispatch(farce.push(routes.home.path))
   }
 
   return next(action)
