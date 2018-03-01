dc79d711169041ca0da0c38047ffadcb10710d9c
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-03-01 10:03:20 +0000

    account: Add createAccount method and change create to decryptAccount on eth-accounts util [Finishes #154736850]

--- app/core/util/eth-accounts.js
+++ app/core/util/eth-accounts.js
@@ -1,5 +1,5 @@
 import { bufferToHex } from 'ethereumjs-util'
-import { fromV3 } from 'ethereumjs-wallet'
+import { generate, fromV3 } from 'ethereumjs-wallet'
 
 /**
  * Remove '0x' prefix from a given string if present
@@ -10,15 +10,40 @@ import { fromV3 } from 'ethereumjs-wallet'
 const removeHexPrefix = str => str.substring(0, 2) === '0x' && str.substring(2)
 
 /**
- * Create an Ethereum account object from an encrypted keystore file
+ * Create a new Ethereum account keystore
+ *
+ * @param {String} password User defined password to encrypt the kesytore
+ * @return {Object}         Encrypted v3 keystore object
+ */
+export const createAccount = password => {
+  try {
+    if (typeof password === 'undefined' || typeof password !== 'string') {
+      return 'Password not set or not in string format'
+    }
+
+    // Create a wallet instance
+    const wallet = generate()
+
+    // Convert wallet to v3 instance, setting the password
+    const v3 = wallet.toV3(password)
+
+    // return the encrypted keystore object
+    return v3
+  } catch (e) {
+    return e
+  }
+}
+
+/**
+ * Decrypt an Ethereum account object from an encrypted keystore file
  *
  * @param  {Object} encryptedKeystore Valid Ethereum keystore file JSON
  * @param  {String} password          Password to decrypt the keystore
  * @return {Object}                   Constructed Ethereum account
  */
-export const create = (encryptedKeystore, password) => {
+export const decryptAccount = (encryptedKeystore, password) => {
   try {
-    // decrypt `keystore` with `password`
+    // decrypt keystore with password
     const decryptedKeystore = fromV3(
       JSON.stringify(encryptedKeystore),
       password,
