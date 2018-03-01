f0d9e6b66b51b9ec2e97bdd86d603147d2ac5a5c
lukehedger <luke.hedger@gmail.com>
2018-03-01 15:53:55 +0000

    Add makeMiddlewareMockFunctions test util

--- test/util/index.js
+++ test/util/index.js
@@ -1,5 +1,6 @@
 import isAsyncAction from './isAsyncAction'
 import makeAsyncAction from './makeAsyncAction'
+import makeMiddlewareMockFunctions from './makeMiddlewareMockFunctions'
 import noop from './noop'
 
-export { isAsyncAction, makeAsyncAction, noop }
+export { isAsyncAction, makeAsyncAction, makeMiddlewareMockFunctions, noop }
