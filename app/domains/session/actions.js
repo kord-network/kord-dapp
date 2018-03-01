9f28a2ddd507d62e1394d1f16aad0107ade7a720
Freddie Tibbles <freddie@tibblesscribbles.com>
2018-03-01 12:12:02 +0000

    misc: Alphabetize function ordering in session actions

--- app/domains/session/actions.js
+++ app/domains/session/actions.js
@@ -1,20 +1,6 @@
 import * as actions from './actionTypes'
 
 /**
- * Start session and set account
- *
- * @param  {Object}  account           		Ethereum account object
- * @param  {String}  account.address   		Public address
- * @param  {String}  account.privateKey		Private key
- * @param  {Object}  keystore          		Encrypted Ethereum keystore object
- * @return {Object}                    		Flux Standard Action
- */
-export const unlockAccount = (account, keystore) => ({
-  type: actions.UNLOCK_ACCOUNT,
-  payload: { account, keystore },
-})
-
-/**
  * Set the newly created user flag
  *
  * @param  {Boolean} isNewUser Newly created user flag
@@ -34,4 +20,18 @@ export const setIsNewUser = isNewUser => ({
 export const setOAuthClaimMessage = oAuthClaimMessage => ({
   type: actions.SET_OAUTH_CLAIM_MESSAGE,
   payload: { oAuthClaimMessage },
+})
+
+/**
+ * Start session and set account
+ *
+ * @param  {Object}  account           		Ethereum account object
+ * @param  {String}  account.address   		Public address
+ * @param  {String}  account.privateKey		Private key
+ * @param  {Object}  keystore          		Encrypted Ethereum keystore object
+ * @return {Object}                    		Flux Standard Action
+ */
+export const unlockAccount = (account, keystore) => ({
+  type: actions.UNLOCK_ACCOUNT,
+  payload: { account, keystore },
 })
