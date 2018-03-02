e1ceeeac166984dde86fe5edc82823b658282a55
lukehedger <luke.hedger@gmail.com>
2018-03-02 17:03:50 +0000

    Add context block to actions tests

--- test/domains/marketplace/actions.spec.js
+++ test/domains/marketplace/actions.spec.js
@@ -3,20 +3,22 @@ import { actions, actionTypes } from 'domains/marketplace'
 
 jest.mock('core/services/marketplace')
 
-describe('domains/ui/actions/GET_MARKETPLACES', () => {
-  it('Should return a standard async action', () => {
-    const actual = isAsyncAction(actions.getMarketplaces())
+describe('domains/marketplace/actions', () => {
+  describe('marketplace/GET_MARKETPLACES action', () => {
+    it('Should return a standard async action', () => {
+      const actual = isAsyncAction(actions.getMarketplaces())
 
-    expect(actual).toBeTruthy()
-  })
+      expect(actual).toBeTruthy()
+    })
 
-  it('Should return action payload', () => {
-    const actual = actions.getMarketplaces()
-    const expected = {
-      promise: new Promise(noop),
-      type: actionTypes.GET_MARKETPLACES,
-    }
+    it('Should return action payload', () => {
+      const actual = actions.getMarketplaces()
+      const expected = {
+        promise: new Promise(noop),
+        type: actionTypes.GET_MARKETPLACES,
+      }
 
-    expect(actual).toEqual(expected)
+      expect(actual).toEqual(expected)
+    })
   })
 })
--- test/domains/ui/actions.spec.js
+++ test/domains/ui/actions.spec.js
@@ -3,32 +3,37 @@ import { isFSA } from 'flux-standard-action'
 import updateActionPayload from './fixtures/update-action-payload.json'
 import { actions, actionTypes } from 'domains/ui'
 
-describe('domains/ui/actions/RESET', () => {
-  it('Should return a Flux Standard Action', () => {
-    const actual = isFSA(actions.reset())
+describe('domains/ui/actions', () => {
+  describe('ui/RESET action', () => {
+    it('Should return a Flux Standard Action', () => {
+      const actual = isFSA(actions.reset())
 
-    expect(actual).toBeTruthy()
+      expect(actual).toBeTruthy()
+    })
+
+    it('Should return action payload', () => {
+      const actual = actions.reset()
+      const expected = { type: actionTypes.RESET }
+
+      expect(actual).toEqual(expected)
+    })
   })
 
-  it('Should return action payload', () => {
-    const actual = actions.reset()
-    const expected = { type: actionTypes.RESET }
+  describe('ui/UPDATE action', () => {
+    it('Should return a Flux Standard Action', () => {
+      const actual = isFSA(actions.update(updateActionPayload))
 
-    expect(actual).toEqual(expected)
-  })
-})
+      expect(actual).toBeTruthy()
+    })
 
-describe('domains/ui/actions/UPDATE', () => {
-  it('Should return a Flux Standard Action', () => {
-    const actual = isFSA(actions.update(updateActionPayload))
+    it('Should return action payload', () => {
+      const actual = actions.update(updateActionPayload)
+      const expected = {
+        type: actionTypes.UPDATE,
+        payload: updateActionPayload,
+      }
 
-    expect(actual).toBeTruthy()
-  })
-
-  it('Should return action payload', () => {
-    const actual = actions.update(updateActionPayload)
-    const expected = { type: actionTypes.UPDATE, payload: updateActionPayload }
-
-    expect(actual).toEqual(expected)
+      expect(actual).toEqual(expected)
+    })
   })
 })
