1c69a7dc74011088d96fec3746e8166dcec6f8c4
lukehedger <luke.hedger@gmail.com>
2018-03-05 15:19:19 +0000

    Update KORD graph service tests with better snapshots

--- test/core/services/kord-graph.spec.js
+++ test/core/services/kord-graph.spec.js
@@ -3,7 +3,6 @@ import { bufferToHex } from 'ethereumjs-util'
 
 import { KordGraph } from 'core/services'
 import createClaimVariables from './fixtures/create-claim-variables.json'
-import createGraphResponse from './fixtures/create-graph-response.json'
 import createGraphVariables from './fixtures/create-graph-variables.json'
 import readClaimsByGraphVariables from './fixtures/read-claims-graph-variables.json'
 import setGraphVariables from './fixtures/set-graph-variables.json'
@@ -16,19 +15,25 @@ describe('core/services/kord-graph/createGraph', () => {
     const address = bufferToHex(generate().getAddress())
 
     // update test data to avoid duplicate graph error
-    createGraphResponse.createGraph.id = address
     createGraphVariables.graph.id = address
 
     const actual = await KordGraph.createGraph(createGraphVariables)
-    const expected = createGraphResponse
 
-    expect(actual).toEqual(expected)
+    // anonymize API response to match snapshot
+    actual.data.createGraph.id = '0x0000000000000000000000000000000000000000'
+    actual.extensions.meta.swarmHash =
+      '0x0000000000000000000000000000000000000000000000000000000000000000'
+
+    expect(actual).toMatchSnapshot()
   })
 })
 
 describe('core/services/kord-graph/setGraph', () => {
   it('Should return successful mutation response', async () => {
     const actual = await KordGraph.setGraph(setGraphVariables)
+
+    // anonymize API response to match snapshot
+    actual.setGraph.id = '0x0000000000000000000000000000000000000000'
 
     expect(actual).toMatchSnapshot()
   })
@@ -38,12 +43,16 @@ describe('core/services/kord-graph/createClaim', () => {
   it('Should return successful mutation response', async () => {
     const actual = await KordGraph.createClaim(createClaimVariables)
 
+    // anonymize API response to match snapshot
+    actual.extensions.meta.swarmHash =
+      '0x0000000000000000000000000000000000000000000000000000000000000000'
+
     expect(actual).toMatchSnapshot()
   })
 })
 
 describe('core/services/kord-graph/readClaimsByGraph', () => {
-  it('Should return successful mutation response', async () => {
+  it('Should return query response', async () => {
     const actual = await KordGraph.readClaimsByGraph(readClaimsByGraphVariables)
 
     expect(actual).toHaveProperty('graph.id')
