f50cf20e4b9db6622370f074cc4bd8c2a02a768b
lukehedger <luke.hedger@gmail.com>
2018-03-06 14:54:27 +0000

    Fix identity domain action test

--- test/domains/identity/actions.spec.js
+++ test/domains/identity/actions.spec.js
@@ -1,18 +1,19 @@
 import { actions, actionTypes } from 'domains/identity'
 import { isAsyncAction, noop } from '~/util'
+import identity from './fixtures/identity.json'
 
 jest.mock('core/services/kord-network')
 
 describe('domains/identity/actions', () => {
   describe('identity/CREATE_IDENTITY action', () => {
     it('Should return a standard async action', () => {
-      const actual = isAsyncAction(actions.createIdentity())
+      const actual = isAsyncAction(actions.createIdentity(identity.address))
 
       expect(actual).toBeTruthy()
     })
 
     it('Should return action payload', () => {
-      const actual = actions.createIdentity()
+      const actual = actions.createIdentity(identity.address)
       const expected = {
         promise: new Promise(noop),
         type: actionTypes.CREATE_IDENTITY,
